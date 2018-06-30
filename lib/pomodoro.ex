c75652ac738d3d3a700a6ad5ce8ab912c6bc9278
Serge Hänni <serge.haenni@simplificator.com>
2018-06-30 13:13:54 +0200

    Added empty nerves project.

---
+++ lib/pomodoro.ex
@@ -0,0 +1,18 @@
+defmodule Pomodoro do
+  @moduledoc """
+  Documentation for Pomodoro.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pomodoro.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
